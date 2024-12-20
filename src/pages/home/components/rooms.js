306507613a441e341c83877201d70b1523207bff
omarderas <bz.inlandtours@gmail.com>
2024-12-20 16:41:20 -0600

    resposiveness

--- src/pages/home/components/rooms.js
+++ src/pages/home/components/rooms.js
@@ -64,7 +64,7 @@ function Rooms() {
 
                               </div>
           
-                          </div> 
+                          </div>                  
                    
 
                       </div>
