b65a38b3edf3cf65ca8b9399895712112238ed6f
omarderas <bz.inlandtours@gmail.com>
2025-02-11 21:29:35 -0600

    resposiveness

--- src/pages/contact-us/components/contact.js
+++ src/pages/contact-us/components/contact.js
@@ -21,8 +21,9 @@ function Contact() {
          We’re here to make your stay unforgettable! Whether you have questions, need assistance with your booking, or want to plan the perfect getaway, our team is ready to help. Reach out to us anytime via phone or email, and we’ll respond promptly. <br />Your dream vacation at Mata Rocks Resort starts here!
          <br /><br />
        </p>
-       <a href="tel:5012262336">Phone: +(501) 226 2336</a>
-         <a href="email:reservations@matarock.com">Email: reservations@matarock.com</a>
+       <a href="tel:5012262336">Office: +(501) 226 2336</a>
+       <a href="https://wa.link/givdq9" target="_blank">WhatsApp: +(501) 226 2336</a>
+       <a href="email:reservations@matarock.com">Email: reservations@matarock.com</a>
         
       </div>
     </div>
