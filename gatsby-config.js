bbf2a6a4aa2b8414a000fb0e713a5849a9716f8a
omarderas <bz.inlandtours@gmail.com>
2025-04-12 15:21:54 -0600

    gtm added

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
